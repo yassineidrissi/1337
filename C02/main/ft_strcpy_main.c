f838e9446ceedab007d07ed7ba9c35842b85d2f5
Yassine Idrissi <yaidriss@c2r1p9.1337.ma>
2022-05-14 20:26:45 +0100

    with C02

---
+++ C02/main/ft_strcpy_main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy_main.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 16:39:47 by yaidriss          #+#    #+#             */
+/*   Updated: 2022/05/14 16:46:21 by yaidriss         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "C02.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(int ac, char **av)
+{
+	char str;
+	char *dst;
+	char *dst1;
+	str = malloc(sizeof(char)*(strlen(av) + 1));
+	dst = malloc(sizeof(char)*(strlen(av) + 1));
+	dst1 = malloc(sizeof(char)*(strlen(av) + 1));
+	ft_str
+	printf("dst of my function is %s", dst);
+	printf("dst of my function is %s", dst);
+}
