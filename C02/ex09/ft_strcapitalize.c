8907a118eb64fa139f5379d0c835b976e954c300
yassine idrissi <yassine42idrissi@gmail.com>
2022-05-15 10:25:11 +0100

    all

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -3,14 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 17:34:55 by yaidriss          #+#    #+#             */
-/*   Updated: 2022/05/14 17:35:51 by yaidriss         ###   ########.fr       */
+/*   Updated: 2022/05/15 02:31:11 by yassine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	ft_strcapitalize(char *str)
+char	*ft_strcapitalize(char *str)
 {
-	
+	int	i;
+
+	i = 1;
+	if ((str[0] >= 'a') && (str[0] <= 'z'))
+		str[0] -= 32;
+	while (str[i])
+	{
+		if ((str[i - 1] < 47) && ((str[i] >= 'a') && (str[i] <= 'z')))
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
 }
--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -3,10 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yaidriss <yaidriss@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yassine <yassine@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/14 17:36:55 by yaidriss          #+#    #+#             */
-/*   Updated: 2022/05/14 17:36:56 by yaidriss         ###   ########.fr       */
+/*   Updated: 2022/05/15 02:32:37 by yassine          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	count;
+	unsigned int	i;
+
+	count = 0;
+	while (src[count] != '\0')
+		++count;
+	i = 0;
+	while (src[i] != '\0' && i < (size - 1))
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = '\0';
+	return (count);
+}
