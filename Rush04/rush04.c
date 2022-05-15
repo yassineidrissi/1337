f1b07693b3f502b50be246ff16d4358b19bf4fe3
Yassine Idrissi <yaidriss@c2r1p9.1337.ma>
2022-05-15 18:37:39 +0100

    with condition

--- Rush04/rush04.c
+++ Rush04/rush04.c
@@ -6,13 +6,47 @@
 /*   By: yaidriss <yaidriss@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 15:40:08 by yaidriss          #+#    #+#             */
-/*   Updated: 2022/05/15 16:57:00 by yaidriss         ###   ########.fr       */
+/*   Updated: 2022/05/15 18:32:24 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char);
+void	ft_putchar(char c);
+
+void	ft_print_first(int x)
+{
+	int	i;
+	i = 0;
+  	if(x == 1)
+	{
+		ft_putchar('A'); 
+		ft_putchar('\n'); 
+	}
+	else
+	{	
+	ft_putchar('A');
+	while(i++ < x -2)
+		ft_putchar('B');
+	ft_putchar('C');
+	ft_putchar('\n');
+	}
+}
+
+
+void ft_print_line(int x)
+{
+  int i;
+  i = 0;
+	if(x > 1)
+	{	
+	ft_putchar('B');
+	while(i++ < x -2)
+	ft_putchar(' ');
+	ft_putchar('B');
+	ft_putchar('\n');
+	}
+}
 
 void ft_print_last(int x)
 {
@@ -32,44 +66,7 @@ void ft_print_last(int x)
   	ft_putchar('\n');
 	}
 }
-
-void ft_print_first(int x)
-{
-  int i;
-  i = 0;
-  	if(x == 1)
-	{
-		ft_putchar('A'); 
-		ft_putchar('\n'); 
-	}
-	else
-	{	
-	ft_putchar('A');
-	while(i++ < x -2)
-		ft_putchar('B');
-	ft_putchar('C');
-	ft_putchar('\n');
-	}
-}
-
-void ft_print_line(int x)
-{
-  int i;
-  i = 0;
-	if(x == 1)
-	{
-		ft_putchar('B'); 
-		ft_putchar('\n'); 
-	}
-	else
-	{	
-	ft_putchar('B');
-	while(i++ < x -2)
-	ft_putchar(' ');
-	ft_putchar('B');
-	ft_putchar('\n');
-	}
-}
+//!we neet to ferifie if we have new line in the last or not
 
 void rush(int x, int y)
 {
@@ -80,6 +77,7 @@ void rush(int x, int y)
   		ft_print_first(x);
   		while(i++ <= y - 2)
     		ft_print_line(x);
-  		ft_print_last(x);
+		if (y != 1)
+  			ft_print_last(x);
  	}
 }
