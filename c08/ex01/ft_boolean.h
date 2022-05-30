09b87a1ea592056e6540f951005cc420ae277e74
yassine idrissi <yassine42idrissi@gmail.com>
2022-05-30 17:39:39 +0100

    test

---
+++ c08/ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lahammam <lahammam@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 12:45:18 by lahammam          #+#    #+#             */
+/*   Updated: 2021/10/12 10:49:28 by lahammam         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define TRUE	1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+# define EVEN(nbr) (nbr) % 2 == 0
+
+typedef int	t_bool;
+#endif
